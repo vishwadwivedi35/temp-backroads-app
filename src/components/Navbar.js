385d73a78b728d7eeeef394838bebcf9afb9fcad
Vishwa Dwivedi <vishwadwivedi35@gmail.com>
2023-12-13 21:35:33 +0530

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,44 @@
+import React from "react";
+import logo from "../images/logo.svg";
+import { pageLinks, socialLinks } from "../data";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} className="nav-logo" alt="backroads" />
+          <button type="button" className="nav-toggle" id="nav-toggle">
+            <i className="fas fa-bars"></i>
+          </button>
+        </div>
+        {/* <!-- left this comment on purpose --> */}
+        <ul className="nav-links" id="nav-links">
+          {pageLinks.map((links) => {
+            return (
+              <li key={links.id}>
+                <a href={links.href} className="nav-link">
+                  {links.text}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+
+        <ul className="nav-icons">
+          {socialLinks.map((link) => {
+            return (
+              <li key={link.id}>
+                <a href={link.href} target="_blank" className="nav-icon">
+                  <i className={link.icon}></i>
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
